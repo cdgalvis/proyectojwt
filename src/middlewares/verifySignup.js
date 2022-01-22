4339bc6a989d84701d0a3790fa33c7e54a1c9898
cdgalvis <cristiangalviscardona@gmail.com>
2022-01-22 08:23:25 -0500

    Fin del VerifySigup

--- src/middlewares/verifySignup.js
+++ src/middlewares/verifySignup.js
@@ -1,6 +1,26 @@
 import User from "../models/User"
 import { ROLES } from "../models/Role"
 
+const checkDuplicateUsernameOrEmail = async (req, res, next) => {
+
+    try {
+        const user = await User.findOne({ username: req.body.username })
+        if(user){
+            return res.status(400).json({message: "The user already exists"})
+        }
+    
+        const email = await User.findOne({ email: req.body.email })
+        if(email){
+            return res.status(400).json({message: "The email already exists"})
+        }
+        
+        next()
+    } catch (error) {
+        res.status(500).json({message: error})
+    }
+
+}
+
 const checkRolesExisted = (req, res, next) => {
 
     if(req.body.roles){
@@ -16,4 +36,4 @@ const checkRolesExisted = (req, res, next) => {
     next()
 }
 
-export { checkRolesExisted }
+export { checkRolesExisted, checkDuplicateUsernameOrEmail }

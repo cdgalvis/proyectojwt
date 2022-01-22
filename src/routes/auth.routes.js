b865c0e629c5a5fac30dbb81c10abb5800decebc
cdgalvis <cristiangalviscardona@gmail.com>
2022-01-22 08:40:53 -0500

    Final Proyecto JWT

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,13 @@ import { verifySignup } from '../middlewares'
 
 const router = Router()
 
+router.use((req, res, next) => {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+    )
+})
+
 router.post('/signup', [verifySignup.checkDuplicateUsernameOrEmail, verifySignup.checkRolesExisted], authCtrl.signUp)
 router.post('/signin', authCtrl.signIn)
 
